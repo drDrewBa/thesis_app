3200182cec208337d32abf18b987de047e065219
drDrewBa <andrew23.gonzaga@gmail.com>
2025-06-30 10:11:06 +0800

    Add fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,45 @@
+import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import "./globals.css";
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Montserrat': require('../assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Thin': require('../assets/fonts/Montserrat-Thin.ttf'),
+    'Montserrat-ThinItalic': require('../assets/fonts/Montserrat-ThinItalic.ttf'),
+    'Montserrat-ExtraLight': require('../assets/fonts/Montserrat-ExtraLight.ttf'),
+    'Montserrat-ExtraLightItalic': require('../assets/fonts/Montserrat-ExtraLightItalic.ttf'),
+    'Montserrat-Light': require('../assets/fonts/Montserrat-Light.ttf'),
+    'Montserrat-LightItalic': require('../assets/fonts/Montserrat-LightItalic.ttf'),
+    'Montserrat-Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Italic': require('../assets/fonts/Montserrat-Italic.ttf'),
+    'Montserrat-Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-MediumItalic': require('../assets/fonts/Montserrat-MediumItalic.ttf'),
+    'Montserrat-SemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-SemiBoldItalic': require('../assets/fonts/Montserrat-SemiBoldItalic.ttf'),
+    'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-BoldItalic': require('../assets/fonts/Montserrat-BoldItalic.ttf'),
+    'Montserrat-ExtraBold': require('../assets/fonts/Montserrat-ExtraBold.ttf'),
+    'Montserrat-ExtraBoldItalic': require('../assets/fonts/Montserrat-ExtraBoldItalic.ttf'),
+    'Montserrat-Black': require('../assets/fonts/Montserrat-Black.ttf'),
+    'Montserrat-BlackItalic': require('../assets/fonts/Montserrat-BlackItalic.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary' : {
+          '50' : '#E6F2FE',
+          '100' : '#CEE5FD',
+          '200' : '#9CCCFC',
+          '300' : '#6BB2FA',
+          '400' : '#3A98F8',
+          '500' : '#087FF7',
+          '600' : '#0765C5',
+          '700' : '#054C94',
+          '800' : '#033363',
+          '900' : '#021931',
+          '950' : '#010D19',
+        },
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+      },
+    },
   },
   plugins: [],
 }
